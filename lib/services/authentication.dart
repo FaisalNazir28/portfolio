0418b1237b4edb034cccd9eea356f9fd444e28df
Faisal Nazir <faisalnazir28@gmail.com>
2024-07-10 19:05:35 +0500

    -Added firebase functionality for users table. -Deploy version -> 2.7

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_portfolio/models/user_model.dart';
 
 class Authentication {
   final auth = FirebaseAuth.instance;
+  final _fireStore = FirebaseFirestore.instance;
 
   Future<void> loginUser({
     required String email,
@@ -17,6 +20,27 @@ class Authentication {
       onSuccess();
     } on FirebaseAuthException catch (e) {
       onError();
+    }
+  }
+
+  Future<void> registerUser() async {
+    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: 'dummy@123.com', password: '123456789');
+
+    if (userCredential.user != null) {
+      var userID = await auth.currentUser!.uid;
+      _fireStore.collection('users').doc(userID).set(UserModel(
+            uid: userID,
+            email: "dummy@123.com",
+            name: "Dummy Me",
+            phone: "555-555-555",
+            designation: "Robot",
+            company: "Anonymous",
+            isActive: false,
+            isAdmin: true,
+          ).toJson());
+    } else {
+      print("User not created!");
     }
   }
 
---
+++ lib/services/firebase_collections.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+class FbCollections {
+  static CollectionReference users = db.collection('users');
+}
