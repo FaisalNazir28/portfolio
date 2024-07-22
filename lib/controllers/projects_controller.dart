c5e5e4a789e4f7828bc6e2a72c1f074317fe2d8c
Faisal Nazir <faisalnazir28@gmail.com>
2024-07-22 15:41:11 +0500

    -projects controller implemented -Deploy version -> 2.7

---
+++ lib/controllers/projects_controller.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_portfolio/models/projects_model.dart';
+import 'package:my_portfolio/services/firebase_collections.dart';
+
+class ProjectsController {
+  Future<List<ProjectsModel>> getAllProjects() async {
+    List<ProjectsModel> allProjects = List<ProjectsModel>.empty(growable: true);
+    QuerySnapshot querySnapshot = await FbCollections.projects.get();
+
+    if (querySnapshot.size > 0) {
+      allProjects = querySnapshot.docs
+          .map((doc) => ProjectsModel.fromJson(doc.data()))
+          .toList();
+    }
+    return allProjects;
+  }
+
+  Future<List<ProjectsModel>> getWebProjects() async {
+    List<ProjectsModel> webProjects = List<ProjectsModel>.empty(growable: true);
+    QuerySnapshot querySnapshot = await FbCollections.projects.get();
+
+    if (querySnapshot.size > 0) {
+      webProjects = querySnapshot.docs
+          .map((doc) => ProjectsModel.fromJson(doc.data()))
+          .where((e) => e.projectPlatform == "Web")
+          .toList();
+    }
+    return webProjects;
+  }
+
+  Future<List<ProjectsModel>> getMobileProjects() async {
+    List<ProjectsModel> mobileProjects =
+        List<ProjectsModel>.empty(growable: true);
+    QuerySnapshot querySnapshot = await FbCollections.projects.get();
+
+    if (querySnapshot.size > 0) {
+      mobileProjects = querySnapshot.docs
+          .map((doc) => ProjectsModel.fromJson(doc.data()))
+          .where((e) => e.projectPlatform == "Mobile")
+          .toList();
+    }
+    return mobileProjects;
+  }
+
+  Future<List<ProjectsModel>> getHybridProjects() async {
+    List<ProjectsModel> hybridProjects =
+        List<ProjectsModel>.empty(growable: true);
+    QuerySnapshot querySnapshot = await FbCollections.projects.get();
+
+    if (querySnapshot.size > 0) {
+      hybridProjects = querySnapshot.docs
+          .map((doc) => ProjectsModel.fromJson(doc.data()))
+          .where((e) => e.projectPlatform == "Hybrid")
+          .toList();
+    }
+    return hybridProjects;
+  }
+}
